0cdf24b02b3cb46f717d1bead4dedd4db21c8eb1
evertonsavio <everluca@outlook.com>
2024-04-12 05:33:53 -0300

    :sparkles: query parameters

--- books.py
+++ books.py
@@ -1,25 +1,36 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
 BOOKS = [
-    {'id': 1, 'title': 'The Da Vinci Code', 'author': 'Dan Brown'},
-    {'id': 2, 'title': 'The Alchemist', 'author': 'Paulo Coelho'},
-    {'id': 3, 'title': 'The Godfather', 'author': 'Mario Puzo'}
+    {'id': 1, 'title': 'The Da Vinci Code', 'author': 'Dan Brown', 'category': 'Fiction'},
+    {'id': 2, 'title': 'The Alchemist', 'author': 'Paulo Coelho', 'category': 'Fiction'},
+    {'id': 3, 'title': 'The Godfather', 'author': 'Mario Puzo', 'category': 'Fiction'},
+    {'id': 4, 'title': 'The Lean Startup', 'author': 'Eric Ries', 'category': 'Non-Fiction'},
 ]
 
 
 @app.get("/books")
-async def read_all_books():
+def read_all_books():
     return {'data': BOOKS}
 
 
 @app.get("/books/{book_id}")
-async def read_book(book_id: int):
+def read_book(book_id: int):
     book = next((book for book in BOOKS if book['id'] == book_id), None)
     return {'data': book}
 
 
+@app.get("/books/")
+def read_category_by_query(category: str):
+    books = [book for book in BOOKS if book['category'].casefold() == category.casefold()]
+    return {'data': books}
+
+
+if __name__ == "__main__":
+    uvicorn.run("books:app", host="127.0.0.1", port=5000, reload=True)
+
 # uvicorn books:app --reload
-# swagger: localhost:8000/docs
+# swagger: localhost:5000/docs
